5d3bf88dd25a1cf99e20fc9bc15a5a095823e1e6
Elef Abner <erosalesq@gmail.com>
2019-08-09 11:20:23 -0500

    a

--- components/blog/Header.vue
+++ components/blog/Header.vue
@@ -17,21 +17,21 @@
 																	<i class="icon icon-close"></i>
 															</div>
 															<li> <a @click="
-																	ga('event', 'Click boton', {'event_category': 'Landing page', 'event_label': 'Precios header', 'value': 1})
+																	ga('event', 'Click boton', {'event_category': 'Header blog', 'event_label': 'Precios header', 'value': 1})
 																	showMenu=false" href="/#plans_section">Precios</a></li>
 															<li> <a @click="
-																	ga('event', 'Click boton', {'event_category': 'Landing page', 'event_label': 'Clientes header', 'value': 1})
+																	ga('event', 'Click boton', {'event_category': 'Header blog', 'event_label': 'Clientes header', 'value': 1})
 																	showMenu=false" href="/#clients">Clientes</a></li>
 
 															<li> <nuxt-link to="/blog">Blog</nuxt-link></li>
 
-															<li> <a @click="
-																	ga('event', 'Click boton', {'event_category': 'Landing page', 'event_label': 'Guia header', 'value': 1})" href="http://guia.easybill.pe" target="_blank">Guia  </a></li>
+															<!-- <li> <a @click="
+																	ga('event', 'Click boton', {'event_category': 'Header blog', 'event_label': 'Guia header', 'value': 1})" href="http://guia.easybill.pe" target="_blank">Guia  </a></li> -->
 															<li>
 																	<a @click="
-																	ga('event', 'Click boton', {'event_category': 'Landing page', 'event_label': 'Ingresar header', 'value': 1})" class="button_login inline button_fill" href="https://app.easybill.pe">Ingresar</a>
+																	ga('event', 'Click boton', {'event_category': 'Header blog', 'event_label': 'Ingresar header', 'value': 1})" class="button_login inline button_fill" href="https://app.easybill.pe">Ingresar</a>
 																	<a @click="
-																	ga('event', 'Click boton', {'event_category': 'Landing page', 'event_label': 'Registrate header', 'value': 1});
+																	ga('event', 'Click boton', {'event_category': 'Header blog', 'event_label': 'Registrate header', 'value': 1});
 																	" href='https://app.easybill.pe/registro' class="button_login button">Regístrate</a>
 															</li>
 													</ul>
