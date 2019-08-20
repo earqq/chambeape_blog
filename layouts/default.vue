1861d097e919833c5fdf86262c1e734cc8c4cea0
Elef Abner <erosalesq@gmail.com>
2019-08-20 16:34:20 -0500

    a

--- layouts/default.vue
+++ layouts/default.vue
@@ -53,3 +53,6 @@ html {
   background-color: #35495e;
 }
 </style>
+<script>
+!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}
+</script>
